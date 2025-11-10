297d6179ba4ff6993eb3212ecc0b36a6a5a61bf7
Kayden M. <kmgarzacarreto1@my.scccd.edu>
2025-11-10 21:48:46 +0000

    prac lab

--- Lab14_3.m
+++ Lab14_3.m
@@ -1,1 +1,11 @@
 
+p = input("Enter the coefficients of a cubic polynomial (y=ax^3+bx^2+cx+d) as a matrix [a b c d]:\n");
+C = input("Enter the integration constant(+C):\n");
+
+DerCoeff = polyder(p);
+IntCoeff = polyint(p,C);
+
+% Report the derivate and integral of the cubic function
+fprintf("The derviative of the cubic polynomial is f'(x) = %.0fx^2+%.0fx+%.0f\n",DerCoeff)
+fprintf("The integral of the cubic polynomial is %.0fx^4+%.0fx^3+%.0fx^2+%.0fx+%.0f\n",IntCoeff)
+
